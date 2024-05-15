a4d756a0906c4ac3946602dc594bc5841ab74cd1
hrsvrn <harshvardhanvatsa@gmail.com>
2024-05-16 01:59:51 +0530

    first commit

---
+++ contestfiles/1971F.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+ 
+void solve()
+{
+    long long r;
+    cin >> r;
+    long long height = r;
+    long long ans = 0;
+    for(long long i = 0; i <= r; i++)
+    {
+        while(i*i+height*height >= (r+1)*(r+1))
+        {
+            height--;
+        }
+        long long cop = height;
+        while(i*i+cop*cop >= r*r && cop > 0)
+        {
+            cop--;
+            ans++;
+        }
+    }
+    cout << ans*4 << endl;
+}
+ 
+int main(){
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
+
