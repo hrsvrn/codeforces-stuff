bade21a9c86467973f7353f8d1c97ac952d41d06
hrsvrn <harshvardhanvatsa@gmail.com>
2024-05-16 09:43:11 +0530

    updated

---
+++ contestfiles/1972a/contest-proporsal.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void problemA()
+{
+  int N=1e6+5;
+  int arr[N],b[N];
+  int n;
+  std::cin >> n;
+  for (int i = 0; i < n; i++) {
+    std::cin >> arr[i];
+  for (int j = 0; j < n; j++) {
+    std::cin >> b[i];
+  int count=0;
+  while(true)
+      {
+        sort(arr,arr+n)
+
+      }
+  }
+  }
+}
